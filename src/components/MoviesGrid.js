07c280771a20a25a646a6e92087c218f5e449317
manojk08 <manojkale303@gmail.com>
2026-04-29 16:14:28 +0530

    show the movies on grid

--- src/components/MoviesGrid.js
+++ src/components/MoviesGrid.js
@@ -5,22 +5,21 @@ function MoviesGrid() {
 
   useEffect(() => {
     fetch("movies.json")
-      .then(res => res.json())
-      .then(data => setMovies(data));
+      .then((res) => res.json())
+      .then((data) => setMovies(data));
   }, []);
 
   return (
-    <div className='movies-grid'>
-      {
-        movies.map(movie => {
+    <div className="movies-grid">
+      {movies.map((movie) => {
         console.log(movie.image);
         return (
-          <div key={movie.id} className='movie-card'>
+          <div key={movie.id} className="movie-card">
             <img src={`images/${movie.image}`} alt={movie.title} />
-            <div className='movie-card-info'>
-              <h3 className='movie-card-title'>{movie.title}</h3>
-              <p className='movie-card-genre'>{movie.genre}</p>
-              <p className='movie-card-rating'>{movie.rating}</p>
+            <div className="movie-card-info">
+              <h3 className="movie-card-title">{movie.title}</h3>
+              <p className="movie-card-genre">{movie.genre}</p>
+              <p className="movie-card-rating">{movie.rating}</p>
             </div>
           </div>
         );
